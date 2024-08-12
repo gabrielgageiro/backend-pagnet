bf4aa1e9e14e919a700164f7929c18798fd000f6
gabrielgageiro <gabrielgageiro@gmail.com>
2024-08-12 19:40:50 -0300

    alteracao do readme e implementacao da tela

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 Desafio usado para estudo do spring batch: [desafio-back-end-pagnet](https://github.com/Pagnet/desafio-back-end/tree/master?tab=readme-ov-file)
 
+TODO: adicoianr link do projeto
+
+# Tecnologias
+- [Spring Boot](https://spring.io/projects/spring-boot)
+- [Spring MVC](https://docs.spring.io/spring-framework/reference/web/webmvc.html)
+- [Spring Data JDBC](https://spring.io/projects/spring-data-jdbc)
+- [Spring Batch](https://spring.io/projects/spring-batch)
+- [Vite](https://vitejs.dev)
+- [React](https://pt-br.react.dev)
+- [Tailwind](https://tailwindcss.com)
+- [PostgreSQL](https://www.postgresql.org/)
--- frontend/frontend-pagnet/src/App.tsx
+++ frontend/frontend-pagnet/src/App.tsx
@@ -1,47 +1,88 @@
-import React, { useState } from 'react';
-import { Button, FileTrigger } from 'react-aria-components';
+import React, { useCallback, useState } from "react";
+import { Button } from "react-aria-components";
+import GitHubButton from "./components/GitHubButton";
 
 function App() {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState<File | null>(null);
   const [transactions, setTransactions] = useState([]);
+  const [isDragging, setIsDragging] = useState(false);
 
-  const handleFileDrop = (files) => {
-    if (files && files.length > 0) {
-      setFile(files[0]);
+  const handleDragEnter = useCallback((e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(true);
+  }, []);
+
+  const handleDragLeave = useCallback((e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(false);
+  }, []);
+
+  const handleDragOver = useCallback((e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }, []);
+
+  const handleDrop = useCallback((e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(false);
+    
+    if (e.dataTransfer?.files[0]) {
+      setFile(e.dataTransfer.files[0]);
+    }
+  }, []);
+  
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target && e.target?.files[0]) {
+      setFile(e.target.files[0]);
     }
   };
-
+  
   return (
     <div className="p-4 max-w-4xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Transações CNAB</h1>
       
+      <GitHubButton href="https://github.com/gabrielgageiro/backend-pagnet"/>
+      
+      <h1 className="text-2xl font-bold mb-4">Transações CNAB</h1>
+
       <div className="flex gap-2 mb-4">
-        <FileTrigger onSelect={handleFileDrop}>
-          <Button className="bg-blue-500 text-white px-4 py-2 rounded">
-            Choose File
-          </Button>
-        </FileTrigger>
-        <Button 
+        <Button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onPress={() => document.getElementById("fileInput")?.click()}
+        >
+          Escolher Arquivo
+        </Button>
+        <Button
           className="bg-blue-500 text-white px-4 py-2 rounded"
           onPress={() => {
+            console.log('enviando arquivo', file)
             // chamar a API para enviar o arquivo
           }}
         >
-          Upload File
+          Enviar Arquivo
         </Button>
+        <input
+          id="fileInput"
+          type="file"
+          className="hidden"
+          onChange={handleFileChange}
+          accept=".txt"
+        />
       </div>
 
-      <FileTrigger onSelect={handleFileDrop}>
-        <div className="border-2 border-dashed p-4 mb-4 border-gray-300 cursor-pointer">
-          {file ? file.name : 'Drag and drop file here'}
-        </div>
-      </FileTrigger>
-
-      <Button className="bg-gray-200 text-gray-700 px-4 py-2 rounded mb-4">
-        Atualizar Transações
-      </Button>
-
-      <div className="mb-4">Espaço para mensagens</div>
+      <div
+        onDragEnter={handleDragEnter}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+        className={`border-2 border-dashed p-4 mb-4 ${
+          isDragging ? "border-blue-500 bg-blue-50" : "border-bla"
+        }`}
+      >
+        {file ? file.name : "Arraste e solte o arquivo aqui"}
+      </div>
 
       <h2 className="text-xl font-bold mb-2">Transações</h2>
       <div className="border rounded overflow-hidden">
